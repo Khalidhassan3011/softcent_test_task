d3dd3a5ad334dd8ab67e4a08825c7a4af685e9d3
khalid <khalidhassan3011@gmail.com>
2022-04-23 00:11:06 +0600

    add translation

--- lib/app/common/language/en_us.dart
+++ lib/app/common/language/en_us.dart
@@ -1,3 +1,20 @@
 const Map<String, String> en = {
+  "appName": "Test Task",
+  "phoneNumber": "Phone Number",
+  "youAgree": "You agree to our ",
+  "termsAndConditions": "Terms & Conditions",
+  "signUpHints": "Don't have an account ? ",
+  "signUp": "Sign in",
+  "verificationCode": "Verification Code",
+  "codeTypeHints": "Please type the code we sent to",
+  "resendHints": "Code Sent. Try again in ",
+  "resend": "Resend",
 
+  // home
+  "welcome": "Welcome to ",
+  "softcent": "Softcent",
+
+  // button
+  "continue": "Continue",
+  "next": "Next",
 };
