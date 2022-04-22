faadfe0a4a2f2c0575cb75609d814ab62ec03454
khalid <khalidhassan3011@gmail.com>
2022-04-23 00:43:09 +0600

    add colors

---
+++ lib/app/common/values/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:softcent_test_task/app/common/util/exports.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color colorPrimary = Color(0xFF2DBC77);
+
+  static const Color colorBg = Color(0xFFE5E5E5);
+  static const Color colorKeyboardBg = Color(0xFFF9F9F9);
+  static const Color colorTextFieldBg = Color(0xFFF8F8F8);
+  static const Color colorOtpFieldBg = Color(0xFFF5F5F5);
+
+  static const Color colorTextBlack = Color(0xFF040415);
+  static const Color colorTextGrey = Color.fromRGBO(4, 4, 21, 0.5);
+  static const Color colorTextTextFiled = Color.fromRGBO(4, 4, 21, 0.6);
+
+  static const Color colorBackButtonBorder = Color.fromRGBO(0, 0, 0, 0.1);
+}
