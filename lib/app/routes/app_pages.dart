bcb884ae5148be0326b6de28e7a30de2b290a727
khalid <khalidhassan3011@gmail.com>
2022-04-23 01:17:27 +0600

    setup main file

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,12 @@
 import 'package:get/get.dart';
 
+import '../modules/home_page/bindings/home_page_binding.dart';
+import '../modules/home_page/views/home_page_view.dart';
+import '../modules/sign_up_page/bindings/sign_up_page_binding.dart';
+import '../modules/sign_up_page/views/sign_up_page_view.dart';
+import '../modules/verification_code_page/bindings/verification_code_page_binding.dart';
+import '../modules/verification_code_page/views/verification_code_page_view.dart';
+
 part 'app_routes.dart';
 
 class AppPages {
@@ -7,5 +14,24 @@ class AppPages {
 
   static const initial = Routes.signUp;
 
-  static final routes = [];
+  static final routes = [
+    GetPage(
+      name: _Paths.signUp,
+      page: () => SignUpPageView(),
+      binding: SignUpPageBinding(),
+      transition: Transition.rightToLeft,
+    ),
+    GetPage(
+      name: _Paths.verificationCode,
+      page: () => VerificationCodePageView(),
+      binding: VerificationCodePageBinding(),
+      transition: Transition.rightToLeft,
+    ),
+    GetPage(
+      name: _Paths.home,
+      page: () => HomePageView(),
+      binding: HomePageBinding(),
+      transition: Transition.rightToLeft,
+    ),
+  ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -3,9 +3,13 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const signUp = _Paths.signUp;
+  static const verificationCode = _Paths.verificationCode;
+  static const home = _Paths.home;
 }
 
 abstract class _Paths {
   _Paths._();
-  static const signUp = '/signUp-page';
+  static const signUp = '/sign-up-page';
+  static const verificationCode = '/verification-code-page';
+  static const home = '/home-page';
 }
